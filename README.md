0714c8e2591fd3498df96c4ae59c6f4aae77923c
Takashi Tamura <tamuratak@users.noreply.github.com>
2016-01-14 16:29:01 +0900

    guess eigen3 header path

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ruby-eigen
-Ruby binding for Eigen using SWIG. At an early development stage.
+Ruby binding for Eigen, a C++ template library for linear algebra, implemented using SWIG.
 
 ## How to install
 
@@ -9,16 +9,28 @@ For Homebrew
 
     $ brew install eigen
     $ gem install rake-compiler
+    $ gem install ruby-eigen
+
+when install fails, try
+
     $ gem install ruby-eigen -- --with-eigen-include=/usr/local/include/eigen3
 
 For MacPorts
 
     $ sudo port install eigen3
     $ gem install rake-compiler
+    $ gem install ruby-eigen
+
+when install fails, try
+
     $ gem install ruby-eigen -- --with-eigen-include=/opt/local/include/eigen3
 
 ### On Ubuntu
 
     $ sudo apt-get install libeigen3-dev
     $ gem install rake-compiler
+    $ gem install ruby-eigen
+
+when install fails, try
+
     $ gem install ruby-eigen -- --with-eigen-include=/usr/include/eigen3
--- ext/eigen/extconf.rb
+++ ext/eigen/extconf.rb
@@ -1,5 +1,34 @@
 require 'mkmf'
-$CXXFLAGS = ($CXXFLAGS || "") + " -O2 -Wall "
-dir_config('eigen')
+eigen3_opt = dir_config('eigen')
 have_library("c++") or have_library("stdc++")
+
+# a monkey patch to make cc_command into cxx_command
+module MakeMakefile
+  def cc_command(opt="")
+    conf = RbConfig::CONFIG.merge('hdrdir' => $hdrdir.quote, 'srcdir' => $srcdir.quote,
+                                  'arch_hdrdir' => $arch_hdrdir.quote,
+                                  'top_srcdir' => $top_srcdir.quote)
+    RbConfig::expand("$(CXX) #$INCFLAGS #$CPPFLAGS #$CXXFLAGS #$ARCH_FLAG #{opt} -c #{CONFTEST_C}",
+                     conf)
+  end
+end
+
+# guess where eigen3 headers unless --with-eigen-include given.
+unless eigen3_opt[0]
+  case RbConfig::CONFIG["includedir"]
+  when /(.*)Cellar\/ruby((?!Cellar).)+\z/   # Homebrew
+    eigen3_path = File.join($1, "include/eigen3")
+  when "/usr/local/include"                 # Ubuntu
+    eigen3_path = "/usr/include/eigen3"
+  else                                      # MacPorts?
+    eigen3_path = File.join(RbConfig::CONFIG["includedir"], "eigen3")
+  end
+
+  if try_compile("#include <Eigen/Core>", "-I" + eigen3_path)
+      $CXXFLAGS = ($CXXFLAGS || "") + " -I#{eigen3_path} "
+  end
+end
+
+
+$CXXFLAGS = ($CXXFLAGS || "") + " -O2 -Wall "
 create_makefile('eigen/eigen')
