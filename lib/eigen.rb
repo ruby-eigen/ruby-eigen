70009ce2b46174162f318fd95ecfb42c53574fac
Takashi Tamura <tamuratak@users.noreply.github.com>
2016-01-14 17:12:39 +0900

    add Eigen::MatrixConstructor

--- lib/eigen.rb
+++ lib/eigen.rb
@@ -1,11 +1,22 @@
 require 'eigen/eigen'
 
-class Eigen::MatrixDouble
-  def self.[](*rows)    
-    ret = new(rows.size, rows[0].size)
-    rows.each_with_index{|rw, i|
-      ret.__set_row__(i, rw)
-    }
-    ret 
+
+module Eigen
+  module MatrixConstructor
+    def [](*rows)
+      ret = new(rows.size, rows[0].size)
+      rows.each_with_index{|rw, i|
+        ret.__set_row__(i, rw)
+      }
+      ret
+    end
   end
+end
+
+class Eigen::MatrixDouble
+  extend Eigen::MatrixConstructor
+end
+
+class Eigen::MatrixComplex
+  extend Eigen::MatrixConstructor
 end
