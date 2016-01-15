227a19204517cc840b9f9a00276b3a497b275e27
Takashi Tamura <tamuratak@users.noreply.github.com>
2016-01-15 17:53:56 +0900

    add hstack,vstack,block_diagonal and their tests

--- lib/eigen.rb
+++ lib/eigen.rb
@@ -7,8 +7,67 @@ module Eigen::MatrixConstructor
     rows.each_with_index{|rw, i|
       ret.__set_row__(i, rw)
     }
-    ret
+    return ret
   end
+
+  def hstack(*ms)
+    col_size = ms.inject(0){|sum, m| m.cols + sum }
+    row_size = ms[0].rows
+    ret = new(row_size, col_size)
+    col_ind = 0
+    ms.each{|m|
+      ret.__set_block__(0,col_ind,m)
+      col_ind += m.cols
+    }
+    return ret
+  end
+
+  def vstack(*ms)
+    row_size = ms.inject(0){|sum, m| m.rows + sum }
+    col_size = ms[0].cols
+    ret = new(row_size, col_size)
+    row_ind = 0
+    ms.each{|m|
+      ret.__set_block__(row_ind,0,m)
+      row_ind += m.rows
+    }
+    return ret
+  end
+
+  def block_diagonal(*ms)
+    row_size = ms.inject(0){|sum, m|
+     if m.respond_to?(:rows)
+       raise(Eigen::EigenRuntimeError, "rows and cols are not equal") unless m.rows == m.cols
+       sum + m.rows
+     elsif m.is_a?(Numeric)
+       sum + 1
+     elsif m.is_a?(Array)
+       sum + m.size
+     else
+       raise Eigen::EigenRuntimeError
+     end
+    }
+    col_size = row_size
+    ret = new(row_size, row_size)
+    ret.setZero
+    row_ind = 0
+    ms.each{|m|
+      if m.respond_to?(:rows)
+        ret.__set_block__(row_ind, row_ind, m)
+        row_ind += m.rows
+      elsif m.is_a?(Array)
+        m0 = self[*m]
+        raise(Eigen::EigenRuntimeError, "rows and cols are not equal") unless m0.rows == m0.cols
+        ret.__set_block__(row_ind, row_ind, m0)
+        row_ind += m.size
+      else
+        ret[row_ind, row_ind] = m
+        row_ind += 1
+      end
+    }
+    return ret
+  end
+
 end
 
 module Eigen::MatrixCommon
--- test/test_eigen.rb
+++ test/test_eigen.rb
@@ -2,16 +2,17 @@ require "test/unit"
 require "eigen"
 
 class TestEigen < Test::Unit::TestCase
+  include Eigen
 
   def test_eigen
-    m = Eigen::MatrixDouble.new(2,2)
+    m = MatrixDouble.new(2,2)
     m[1,0] = 2
     assert_equal(m[1,0], 2)
     assert m.eigenvalues()[0].is_a?(Complex)
   end
 
   def test_matrix
-    m = Eigen::MatrixDouble.new(2,2)
+    m = MatrixDouble.new(2,2)
     k = 79.2
     m[0,1] = k; m[1,1] = 2
     assert_equal(k, m.col(1)[0])
@@ -21,21 +22,21 @@ class TestEigen < Test::Unit::TestCase
   end
 
   def test_matrix02
-    m1 = Eigen::MatrixDouble.new(200,200)
+    m1 = MatrixDouble.new(200,200)
     m1.setRandom
-    m2 = Eigen::MatrixDouble.new(200,200)
+    m2 = MatrixDouble.new(200,200)
     m2.setRandom
     m1 * m2
   end
 
   def test_matrix03
-    m1 = Eigen::MatrixDouble.new(2,3)
-    m2 = Eigen::MatrixDouble.new(4,5)
-    assert_raise(Eigen::EigenRuntimeError){ m1*m2 }
+    m1 = MatrixDouble.new(2,3)
+    m2 = MatrixDouble.new(4,5)
+    assert_raise(EigenRuntimeError){ m1*m2 }
   end
 
   def test_matrix04
-    m = Eigen::MatrixDouble.new(2,2)
+    m = MatrixDouble.new(2,2)
     m.setRandom
     m[0,1] = 1
     assert_equal(0, m.tril[0,1])
@@ -43,7 +44,7 @@ class TestEigen < Test::Unit::TestCase
   end
 
   def test_matrix05
-    m = Eigen::MatrixDouble.new(2,2)
+    m = MatrixDouble.new(2,2)
 
     m.setRandom
     lu = m.fullPivLu
@@ -57,11 +58,11 @@ class TestEigen < Test::Unit::TestCase
   end
 
   def test_matrix06
-    m = Eigen::MatrixDouble.new(2,2)
+    m = MatrixDouble.new(2,2)
     m.setZero
     m.__set_col__(0, [1,2])
     m.__set_row__(0, [1,2])
-    m = Eigen::MatrixDouble[[1,2], [3,4]]
+    m = MatrixDouble[[1,2], [3,4]]
 
     qr = m.fullPivHouseholderQR
     assert  ( m * qr.p ).isApprox( qr.q * qr.r )
@@ -75,25 +76,63 @@ class TestEigen < Test::Unit::TestCase
   end
 
   def test_matrix07
-    m = Eigen::MatrixDouble.new(1,2)
-    v = Eigen::VectorDouble.new(2)
+    m = MatrixDouble.new(1,2)
+    v = VectorDouble.new(2)
     m * v
     v * m
 
-    vc = Eigen::VectorComplex.new(2)
+    vc = VectorComplex.new(2)
     vc.setRandom
     vc.segment(0,1)
   end
 
   def test_matrix08
-    m = Eigen::MatrixDouble.new(3,3)
+    m = MatrixDouble.new(3,3)
     m.setOnes
     n = m.__get_block__(1,1,2,2)
     n[0,0] = 0
   end
 
+  def test_matrix09
+    m = MatrixDouble.new(3,3)
+    m.setOnes
+    assert_equal(m.to_a, Array.new(3){ [1.0, 1.0, 1.0] })
+
+    v = VectorDouble[1,2,3]
+    assert_equal(v.to_a, [1.0, 2.0, 3.0])
+  end
+
+  def test_matrix_stack
+    m1 = MatrixDouble.new(3,3)
+    m1.setOnes
+    m2 = MatrixDouble.new(3,3)
+    m2.setZero
+    assert_equal(MatrixDouble.hstack(m1, m2),
+                 MatrixDouble[ *Array.new(3){ [1, 1, 1, 0, 0, 0] } ])
+
+    assert_equal(MatrixDouble.vstack(m1, m2),
+                 MatrixDouble[ *(Array.new(3){ [1, 1, 1] } + Array.new(3){ [0, 0, 0] }) ])
+
+    assert_equal(MatrixDouble.block_diagonal(1,2,3),
+                 MatrixDouble[ [1,0,0], [0,2,0], [0,0,3] ])
+
+    m3 = MatrixDouble.new(2,2)
+    m3.setOnes
+    assert_equal(MatrixDouble.block_diagonal(m3,2),
+                 MatrixDouble[ [1,1,0], [1,1,0], [0,0,2] ])
+
+    assert_raise(EigenRuntimeError){ MatrixDouble.block_diagonal(MatrixDouble.new(2,1),2) }
+
+    assert_equal(MatrixDouble.block_diagonal([[1,1],[1,1]],1),
+                 MatrixDouble[ [1,1,0], [1,1,0], [0,0,1] ])
+
+    assert_equal(MatrixComplex.block_diagonal([[1i,1],[1,1]],1+1i),
+                 MatrixComplex[ [1i,1,0], [1,1,0], [0,0,1+1i] ])
+
+  end
+
   def test_vector01
-    assert_nothing_raised{ Eigen::VectorDouble[1, 2, 3] }
+    assert_nothing_raised{ VectorDouble[1, 2, 3] }
   end
 
 end
