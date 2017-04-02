a3fb09c95d8bd5ea321b76fb460d9451fa9bdfe7
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-04-02 15:18:26 +0900

    delete unused code

--- ext/eigen/extconf.rb
+++ ext/eigen/extconf.rb
@@ -3,38 +3,10 @@ eigen3_opt = dir_config('eigen')
 have_library("c++") or have_library("stdc++")
 
 unless eigen3_opt[0]
-  eigen3_path = File.join( File.dirname(File.expand_path(__FILE__)), "eigen")
-  $CXXFLAGS = ($CXXFLAGS || "") + " -I#{eigen3_path} "
+  eigen_path = File.join( File.dirname(File.expand_path(__FILE__)), "eigen")
+  find_header("Eigen/src/misc/blas.h", eigen_path)
 end
+have_func("rb_gc_adjust_memory_usage")
 
-$CXXFLAGS = ($CXXFLAGS || "") + " -std=c++11 -Wall "
+$CXXFLAGS = ($CXXFLAGS || "") + " -std=c++11 "
 create_makefile('eigen/eigen')
-
-
-# a monkey patch to make cc_command into cxx_command
-# module MakeMakefile
-#   def cc_command(opt="")
-#     conf = RbConfig::CONFIG.merge('hdrdir' => $hdrdir.quote, 'srcdir' => $srcdir.quote,
-#                                   'arch_hdrdir' => $arch_hdrdir.quote,
-#                                   'top_srcdir' => $top_srcdir.quote)
-#     RbConfig::expand("$(CXX) #$INCFLAGS #$CPPFLAGS #$CXXFLAGS #$ARCH_FLAG #{opt} -c #{CONFTEST_C}",
-#                      conf)
-#   end
-# end
-
-# guess where eigen3 headers unless --with-eigen-include given.
-
-# unless eigen3_opt[0]
-#   case RbConfig::CONFIG["includedir"]
-#   when /(.*)Cellar\/ruby((?!Cellar).)+\z/   # Homebrew
-#     eigen3_path = File.join($1, "include/eigen3")
-#   when "/usr/local/include"                 # Ubuntu
-#     eigen3_path = "/usr/include/eigen3"
-#   else                                      # MacPorts?
-#     eigen3_path = File.join(RbConfig::CONFIG["includedir"], "eigen3")
-#   end
-
-#   if try_compile("#include <Eigen/Core>", "-I" + eigen3_path)
-#       $CXXFLAGS = ($CXXFLAGS || "") + " -I#{eigen3_path} "
-#   end
-# end
