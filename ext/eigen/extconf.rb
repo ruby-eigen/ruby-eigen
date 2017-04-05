e829f73789c692248f6a934b1d9bc49fda40df7a
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-04-05 09:42:35 +0900

    try link to blas and lapack libraries

--- ext/eigen/extconf.rb
+++ ext/eigen/extconf.rb
@@ -6,13 +6,28 @@ gems = Gem::Specification.find_all_by_name("numo-narray")
 if gems.size > 1
   raise "multiple numo-narray gems found"
 end
-find_header("numo/narray.h", File.join(gems[0].gem_dir, "lib/numo/") )
+unless find_header("numo/narray.h", File.join(gems[0].gem_dir, "lib/numo/") )
+  raise "numo-narray required"
+end
 
 unless eigen_opt[0]
   eigen_path = File.join( File.dirname(File.expand_path(__FILE__)), "eigen")
   find_header("Eigen/src/misc/blas.h", eigen_path)
 end
+
 have_func("rb_gc_adjust_memory_usage")
 
-$CXXFLAGS = ($CXXFLAGS || "") + " -std=c++11 "
+backend_flag = ""
+if enable_config("blas", true)
+ if have_library("OpenBLAS") or have_library("atlas")
+   backend_flag += " -DEIGEN_USE_BLAS "
+ end
+end
+
+if backend_flag == "" and enable_config("lapacke", true) and find_library("lapacke", nil, "/opt/local/lib/lapack/")
+   have_framework("Accelerate")
+   backend_flag += " -DEIGEN_USE_LAPACKE "
+end
+
+$CXXFLAGS = ($CXXFLAGS || "") + " -std=c++11 #{backend_flag} "
 create_makefile('eigen/eigen')
